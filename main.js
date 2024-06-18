d8723d1b6fece9614e5d4739bf5cf03be5d05f2c
Jorge <jjptni@gmail.com>
2024-06-17 21:09:22 -0600

    cajero

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+
+function login(form){
+    if(form.u.value === "maria@gmail.com"){
+        if(form.c.value === "password"){
+            location="home.html";
+        }else {
+           // alert("error de password");
+          
+        }
+    }else{
+        alert("error de usuario");       
+        }
+    }
+
+
+
+
---
+++ main1.js
@@ -0,0 +1,30 @@
+function formula(){
+    // let cantidadT = parseFloat(document.getElementById('cant').value);
+    //let  cantidad1 = parseFloat(document.getElementById('c').value);
+    //document.getElementById('c').value=(cantidad1-cantidadT);
+
+    let cantidadT = parseFloat(document.querySelector('#cant').value);
+    let  cantidad1 = parseFloat(document.querySelector('#c').value);
+    document.querySelector('#c').value=(cantidad1-cantidadT);
+}
+
+function formula2(){
+    // let cantidadT = parseFloat(document.getElementById('cant').value);
+    //let  cantidad1 = parseFloat(document.getElementById('c').value);
+    //document.getElementById('c').value=(cantidad1-cantidadT);
+
+    let cantidadT = parseFloat(document.querySelector('#cant').value);
+    let  cantidad1 = parseFloat(document.querySelector('#c').value);
+    document.querySelector('#c').value=(cantidad1-cantidadT);
+}
+
+
+function formula3(){
+    // let cantidadT = parseFloat(document.getElementById('cant').value);
+    //let  cantidad1 = parseFloat(document.getElementById('c').value);
+    //document.getElementById('c').value=(cantidad1-cantidadT);
+
+    let cantidadT = parseFloat(document.querySelector('#cant').value);
+    let  cantidad1 = parseFloat(document.querySelector('#c').value);
+    document.querySelector('#c').value=(cantidad1+cantidadT);
+}
